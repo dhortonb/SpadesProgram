153ac27d81c426f02e89495a2a2b9d1d07e452b0
Donez Horton-Bailey <dhortonbailey@gmail.com>
2013-08-11 22:07:34 -0700

    Created: die(), class Card, typedef Number, typedef Suit

---
+++ README.md
@@ -0,0 +1,2 @@
+So, this is my own side project to practice coding in C++ and using object 
+oriented programming. I will be programming the game spades. Wish me luck.
--- objects.cpp
+++ objects.cpp
@@ -2,10 +2,61 @@ using namespace std;
 #include <iostream>
 #include <cerrno>
 
-typedef enum {
-    'A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'
-} number;
+void die(const char *message)
+{
+    if (errno) {
+        perror (message);
+    } else {
+        cout << "ERROR: " << message << endl;
+    }
+    exit(1);
+}
 
 typedef enum {
-    HEARTS, DIAMONDS, CLUBS, SPADES
-} suit;
+    A=1, 
+    II=2, 
+    III=3, 
+    IV=4, 
+    V=5, 
+    VI=6, 
+    VII=7, 
+    VIII=8, 
+    IX=9, 
+    X=10, 
+    J=11, 
+    Q=12, 
+    K=13
+} Number;
+
+typedef enum {
+    HEARTS=0, DIAMONDS=1, CLUBS=2, SPADES=3
+} Suit;
+
+class Card
+{
+public:
+    Number number;
+    Suit suit;
+    int index;
+
+    Card (int nu = A, int su = SPADES, int i = 0)
+    {
+        if (nu < 1 || nu > 13) die ("Number out of bounds");
+        if (su < 0 || su > 3) die ("Suit out of bounds");
+        number = static_cast<Number>(nu);
+        suit = static_cast<Suit>(su);
+        index = i;
+    }
+};
+
+int main()
+{
+    // Testing the card creation.
+    Card c1(12, 2, 3);
+    Card c2(3, 3, 49);
+    Card c3(9,2, 21);
+    cout << "Card 1: " << c1.number << "," << c1.suit << "," << c1.index << endl;
+    cout << "Card 2: " << c2.number << "," << c2.suit << "," << c2.index << endl;
+    cout << "Card 3: " << c3.number << "," << c3.suit << "," << c3.index << endl;
+    return 0;
+}
